91942130e82c023249d6ad81e697428478de06b7
reece <rwdigiacomo01@bvsd.org>
2022-09-17 16:28:40 -0600

    Archimedian Spiral function implemented with python

---
+++ spiral.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import unicodedata
+radius = 1
+tlist = np.linspace(0, 15, num=1000)
+
+def x(t):
+    return (radius/(2*np.pi) * t) * np.cos(t)
+def y(t):
+    return (radius/(2*np.pi) *t) * np.sin(t)
+
+xlist = x(tlist)
+ylist = y(tlist)
+
+plt.figure(num=0, dpi=120)
+plt.plot(xlist,ylist)
+plt.grid(True)
+
+pi = unicodedata.lookup("GREEK SMALL LETTER PI")
+
+plt.xlabel("((r/2"+pi+")*t)*cos(t)")
+plt.ylabel("((r/2"+pi+")*t)*sin(t)")
+plt.title("Archimedean Spiral")
+
+plt.show()
