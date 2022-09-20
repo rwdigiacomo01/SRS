b3694abfd312d42c793cd5835cfa3e5903c7ffd4
rwdigiacomo01 <rwdigiacomo01@bvsd.org>
2022-09-20 14:29:08 -0600

    Added a commented out section that creates a Parker Spiral

--- spiral.py
+++ spiral.py
@@ -9,11 +9,23 @@ def x(t):
 def y(t):
     return (radius/(2*np.pi) *t) * np.sin(t)
 
+
 xlist = x(tlist)
 ylist = y(tlist)
 
+# This commented out section adds an Archimedean Spiral in the other direction
+# Creating a Parker Spiral
+# def c(t):
+#    return (-1*radius/(2*np.pi) * t) * np.cos(t)
+# def v(t):
+#    return (-1*radius/(2*np.pi) *t) * np.sin(t)
+
+# clist = c(tlist)
+# vlist = v(tlist)
+
 plt.figure(num=0, dpi=120)
 plt.plot(xlist,ylist)
+# plt.plot(clist, vlist)
 plt.grid(True)
 
 pi = unicodedata.lookup("GREEK SMALL LETTER PI")
