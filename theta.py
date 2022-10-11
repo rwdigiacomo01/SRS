da285f9d7a155e84969547b6faa07a4d7189417d
rwdigiacomo01 <rwdigiacomo01@bvsd.org>
2022-10-11 15:03:26 -0600

    Not finished pretty sure that rxdist in unnecessary. Need the change in x points and change in y points and the y point given from the radial slope.

--- theta.py
+++ theta.py
@@ -2,6 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+radius = 1
+tlist = np.linspace(0, (2 * np.pi), num=1000)
+
+def x(t):
+    return (radius / (2 * np.pi) * t) * np.cos(t)
+
+
+def y(t):
+    return (radius / (2 * np.pi) * t) * np.sin(t)
+
+
+xlist = x(tlist)
+ylist = y(tlist)
 
 def getAngle(xdist, ydist, rxdist, rydist):
     vector_a = float(np.sqrt(((xdist ** 2) + (ydist ** 2))))
@@ -11,4 +24,13 @@ def getAngle(xdist, ydist, rxdist, rydist):
     theta = math.acos(ratio) * (360 / (2 * np.pi))
 
     return theta
+
+for i in range(len(xlist)):
+    magnitude = np.sqrt(((xlist[i+1] - xlist[i])**2) + ((ylist[i+1] - ylist[i])**2))
+    rslope = ylist/xlist
+    ry = rslope*xlist[i+1]
+    rmagnitude = np.sqrt(((xlist[i + 1] - xlist[i]) ** 2) + ((ry - ylist[i]) ** 2))
+
+
+
 
