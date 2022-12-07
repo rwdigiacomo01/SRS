569cc736046bd084047697665a4aa049be43a36b
rwdigiacomo01 <rwdigiacomo01@bvsd.org>
2022-12-07 14:41:38 -0700

    Plotted HCI coordinates using DatPlot.py make sure to change np.loadtxt references so that file is accessible

--- Data/DatPlot.py
+++ Data/DatPlot.py
@@ -2,43 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-data = np.loadtxt(r'C:\Users\reece\Downloads\B_1s_orb1')
-
+data = np.loadtxt(r'D:\Copy of r_psp_hci_1s_orb1')
+data2 = np.loadtxt(r'D:\Copy of r_psp_hci_1s_orb2')
+data4 = np.loadtxt(r'D:\Copy of r_psp_hci_1s_orb4')
 
 t = []
 x = []
 y = []
 z = []
 
-i = 456839
-print((data[456839])[2])
-while i < 457839:
-    t.append((data[i])[0])
-    x.append((data[i])[1])
-    y.append((data[i])[2])
-    z.append((data[i])[3])
-    i+=1
+i = 1279000
 
-#i = 0
+while i < 5270001:
+   t.append((data[i])[0])
+   x.append((data[i])[1]/ (1.496*10**8))
+   y.append((data[i])[2]/ (1.496*10**8))
+   z.append((data[i])[3]/ (1.496*10**8))
+   i+=500
 
-#while i < len(data):
-   # t.append((data[i])[0])
-   # x.append((data[i])[1])
-   # y.append((data[i])[2])
-   # z.append((data[i])[3])
-   # i+=1
+# i = 0
+# #print(len(data))
+# while i < len(data):
+#     t.append((data[i])[0])
+#     x.append((data[i])[1])
+#     y.append((data[i])[2])
+#     z.append((data[i])[3])
+#     i+=1
 
-#i = 0
-#spot = 0
-#while i < len(data):
-   # if(str(x[i]) != "nan"):
-   #     spot = i
-   #     print(spot)
-   #     break
-   # else:
-   #     i+=1
+# i = 0
+# spot = 0
+# while i < len(data):
+#     if(str(x[i]) != "nan"):
+#         spot = i
+#         print(spot)
+#         break
+#     else:
+#         i+=1
 
 ax = plt.axes(projection="3d")
+
 ax.scatter(x, y, z)
+
+ax.set_xlim(-.6, .6)
+ax.set_ylim(-.6, .6)
+ax.set_zlim(-.6, .6)
 
 plt.show()
--- solar.py
+++ solar.py
@@ -13,24 +13,24 @@ for i in range(m):
         angle.append(0)
     angle.append(angle[i]+((360/m)*(np.pi/180)))
 #temp solution
+# I kept getting 1 more value than I needed so I popped the last value
 angle.pop()
 
-print(angle)
-print(len(angle))
+
 def solar (t):
     return t * ((400)/(1.496*10**8))
 
 slist = solar(tlist)
-#print(slist)
-
-r = np.arange(0, 2, 0.01)
-theta = 2 * np.pi * r
 
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.plot(angle, slist)
-# ax.set_rmax(2)
+
+#ax.set_rmax(1)
+# use this to zoom in by setting the rmax to the needed AU value
+
 # ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
 # ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+
 ax.grid(True)
 
 ax.set_title("The Travel of Solar Wind in one Solar Rotation", va='bottom')
